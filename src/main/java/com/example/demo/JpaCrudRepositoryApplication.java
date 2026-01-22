535e1f9c09f689178acebae76cfc1d7035bf786c
Ritesh Singh <Ritesh.Singh@syenainfosoft.com>
2026-01-22 14:08:34 +0530

    codes commited for jpa filters entity

---
+++ src/main/java/com/example/demo/JpaCrudRepositoryApplication.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@SpringBootApplication
+public class JpaCrudRepositoryApplication implements WebMvcConfigurer{
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaCrudRepositoryApplication.class, args);
+	}
+	
+	@Override
+		public void addCorsMappings(CorsRegistry registry) {
+		    registry.addMapping("/**")
+		            .allowedOrigins("http://localhost:64092") // Specify the allowed origin (your frontend URL)
+		            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Specify the allowed HTTP methods
+		            .allowedHeaders("*") // Allow any headers
+		            .allowCredentials(true) // Allows credentials such as cookies or HTTP authentication
+		            .maxAge(3600); // Cache pre-flight response for 1 hour (in seconds)
+		}
+
+}
---
+++ src/main/java/com/example/demo/service/CompanyService.java
@@ -0,0 +1,46 @@
+package com.example.demo.service;
+import java.util.Map;
+import java.util.Optional;
+
+import com.example.demo.entity.Company;
+import com.example.demo.entity.Customers;
+import com.example.demo.entity.Workers;
+import com.example.demo.pojo.CompanyDetails;
+
+public interface CompanyService {
+
+public Company saveCompanyInfo(Company company);
+	
+	public CompanyDetails getCompanyList();
+	
+	public CompanyDetails getCompanyDetailsList();
+	
+	public Optional<Company> getCompanyById(long companyId);
+	
+	public Map<String,String> deleteCompanyById(long companyId);
+	
+	public Map<String,String> updateCompany(Company company);
+	
+	public Map<String,String> getDuplicateCompany(long companyId);
+	
+	public Workers saveWorkersData(Workers worker); 
+	
+	public CompanyDetails getWorkersList();
+
+	public Optional<Workers> getWorkersById(long workerId);
+	
+	public Map<String,String> deleteWorkerById(long workerId);
+	
+	public Map<String,String> updateWorkers(Workers worker);
+	
+	public Customers saveCustomersData(Customers customers);
+	
+	public CompanyDetails getCustomersList();
+	
+	public  Optional<Customers> getCustomerById(long customerId);
+	
+	public Map<String,String> deleteCustomerById(long customerId);
+	
+	public Map<String,String> updateCustomers(Customers customers);
+	
+}
