d0f0e1017ef2b8b7bcae2eaa4337dc9869395a64
Ritesh Singh <Ritesh.Singh@syenainfosoft.com>
2026-02-16 15:57:30 +0530

    classic cross origin added

--- src/main/java/com/example/demo/controller/CompanyController.java
+++ src/main/java/com/example/demo/controller/CompanyController.java
@@ -2,6 +2,7 @@ package com.example.demo.controller;
 import java.util.Map;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,6 +18,7 @@ import com.example.demo.service.CompanyService;
 
 @RestController
 @RequestMapping("company")
+@CrossOrigin(origins = "http://localhost:4200")
 public class CompanyController {
 	
 	@Autowired
